338c9e3a716cf0a64507d1939cdafe004965fa2e
Sean Kunz <skunz1@binghamton.edu>
2022-02-22 21:08:27 -0500

    computed first four calcs

--- proj1/project01.c
+++ proj1/project01.c
@@ -1,15 +1,90 @@
 #include "project01.h"
 
+int read_input(const char * filename, int *input_array, int array_length) {
+    FILE *fp;
+    int line_num = 0;
+    int line_length = 255;
+    char line[line_length];
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("Could not open file %s", filename);
+        return -1;
+    }
+
+    while (fgets(line, line_length, fp)) {
+        if (strcmp(line, "No-Request\n") == 0) {
+            input_array[line_num] = NO_REQUEST;
+            no_request++;
+        } else if (strcmp(line, "Bake-Bagel\n") == 0) {
+            input_array[line_num] = BAKE_BAGEL;
+            bagel_baked++;
+        } else if (strcmp(line, "Bake-Baguette\n") == 0) {
+            input_array[line_num] = BAKE_BAGUETTE;
+            baguette_baked++;
+        } else {
+            input_array[line_num] = INVALID_REQUEST;
+        }
+        line_num++;
+    }
+
+    fclose(fp);
+    return 0;
+}
+
+int count_lines(const char * filename) {
+    int num_lines;
+    FILE *fp;
+    char c;
+
+    fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Could not open file %s", filename);
+        return -1;
+    }
+
+    for (c = getc(fp); c != EOF; c = getc(fp)) {
+        if (c == '\n') {
+            num_lines++;
+        }
+    }
+
+    fclose(fp);
+    return num_lines;
+}
+
 int main(int argc, char *argv[])  {
+    if (argc != 2) {
+        printf("Input in the form: ./baking_sim <trace_filename>\n");
+        return -1;
+    }
+
+    const char * filename = argv[1];
+
+    baking_count = count_lines(filename);
+    if (baking_count < 0) {
+        return -1;
+    }
+
+    no_request = 0;
+    bagel_baked = 0;
+    baguette_baked = 0;
+    
+    printf("%d\n", baking_count);
+
+    int input_array[baking_count];
+    read_input(filename, input_array, baking_count);
+
     //output formats
     printf("\nBaking count: %d\n", baking_count);
     printf(" - Bagel baked: %d\n", bagel_baked);
     printf(" - Baguette baked: %d\n", baguette_baked);
     printf("No request: %d\n", no_request);
 
-    printf("\nHow many minutes to bake: %d\n", bakery_time);
+    /*printf("\nHow many minutes to bake: %d\n", bakery_time);
 
-    printf("\nPerformance (bakes/minutes): %.2f\n", performance);
+    printf("\nPerformance (bakes/minutes): %.2f\n", performance);*/
 
     return 0;
 }
--- proj1/project01.h
+++ proj1/project01.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int bakery_time;
 int no_request;
@@ -6,3 +8,10 @@ int bagel_baked;
 int baguette_baked;
 int baking_count;
 float performance;
+
+enum RequestType {
+    NO_REQUEST=0,
+    BAKE_BAGEL=1,
+    BAKE_BAGUETTE=2,
+    INVALID_REQUEST=-1
+};
